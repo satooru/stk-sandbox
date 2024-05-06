16eff291114fae9aacf551a7b40a0f8b81df070a
satooru <16670493+satooru@users.noreply.github.com>
2024-05-06 20:29:40 -0300

    sample python script

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# this is a sample python script
+#
+# I`d like to split this python script using stackspot
+#
+# Each stackspot plugin would have a function and the print accordingly
+
+def sum(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+print(sum(1,1))
+print(subtract(1,1))
+print(multiply(1,1))
+print(divide(1,1))
